0eadd7e9e19d562aa660cdca967d5fe5d9af688d
Alex D. Zharichenko <azharichenko@gmail.com>
2020-05-24 12:28:39 -0400

    Added rest of script, now runs on hourly cycle

--- watcher.py
+++ watcher.py
@@ -1,40 +1,38 @@
 import argparse
 import csv
 import sched
+from datetime import datetime, timedelta
 from pathlib import Path
 
 from requests.auth import HTTPBasicAuth
 from gql import gql, Client
 from gql.transport.requests import RequestsHTTPTransport
 
-
 parser = argparse.ArgumentParser(description="")
 parser.add_argument("--user", type=str)
 parser.add_argument("--token", type=str)
-parser.add_argument('--repo', type=str)
-parser.add_argument('--owner', type=str)
+parser.add_argument("--repo", type=str)
+parser.add_argument("--owner", type=str)
 
 args = parser.parse_args()
 
+hour = timedelta(hours=1)
 
-output_csv = Path('.') / 'output.csv'
-
+output_csv = Path(".") / "output.csv"
 
 github_transport = RequestsHTTPTransport(
-    url='https://api.github.com/graphql',
-    headers={'Authorization': 'token'},
-    auth=HTTPBasicAuth(args.user, args.token)
+    url="https://api.github.com/graphql",
+    headers={"Authorization": "token"},
+    auth=HTTPBasicAuth(args.user, args.token),
 )
 
-cycle = {
-    "hourly": 60 * 60,
-    "daily": 60 * 60 * 24
-}
+s = sched.scheduler()
 
 
 def stargazer_count(owner: str, repo_name: str) -> int:
     client = Client(transport=github_transport, fetch_schema_from_transport=True)
-    query = gql('''
+    query = gql(
+        """
         query {{ 
         repository(name: "{repo}", owner: "{owner}") {{
         stargazers {{
@@ -42,13 +40,44 @@ def stargazer_count(owner: str, repo_name: str) -> int:
         }}
       }}
     }}
-    '''.format(repo=repo_name, owner=owner))
-    return client.execute(query)['repository']['stargazers']['totalCount']
+    """.format(
+            repo=repo_name, owner=owner
+        )
+    )
+    return client.execute(query)["repository"]["stargazers"]["totalCount"]
 
 
-def write_new_stargazer_entry():
-    with output_csv.open('w') as csv_file:
-        pass
+def initalize_csv():
+    with output_csv.open("w", newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(['datetime', 'stargazers'])
 
 
-print(stargazer_count(args.owner, args.repo))
+def write_new_stargazer_entry(data):
+    with output_csv.open("a", newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(data)
+
+
+def calculate_next_hour():
+    next_hour = datetime.today() + hour
+    next_hour = next_hour - timedelta(
+        microseconds=next_hour.microsecond,
+        seconds=next_hour.second,
+        minutes=next_hour.minute,
+    )
+    delta = next_hour - datetime.today()
+    return delta.seconds
+
+
+def main():
+    data = [datetime.today().isoformat(), stargazer_count(args.owner, args.repo)]
+    write_new_stargazer_entry(data)
+    s.enter(calculate_next_hour(), 1, main)
+
+
+
+if not output_csv.exists():
+    initalize_csv()
+s.enter(0, 1, main)
+s.run()
